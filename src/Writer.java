b3707b6f5a827646c874f3ca2678fda55800b2ac
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-22 14:22:38 -0700

    Implemented Writer.java

--- src/Writer.java
+++ src/Writer.java
@@ -1,3 +1,27 @@
-public class Writer
+public class Writer extends Person
 {
+    private String favBook;
+
+    public Writer() {}
+
+    public Writer(String name, int ssn, String favBook)
+    {
+        super(name, ssn);
+        this.favBook = favBook;
+    }
+
+    public String getFavBook()
+    {
+        return favBook;
+    }
+    public void setFavBook(String favBook)
+    {
+        this.favBook = favBook;
+    }
+
+    @Override
+    public void greet()
+    {
+        System.out.printf("Hello, you may call me %s. I consider myself to be a writer. My favourite book is %s.\n", this.getName(), this.getFavBook());
+    }
 }
