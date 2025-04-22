a5c2558740bee39782536cd5e9e8e44abdf81aec
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-22 14:20:44 -0700

    Implemented Gamer.java

--- src/Gamer.java
+++ src/Gamer.java
@@ -1,3 +1,27 @@
-public class Gamer
+public class Gamer extends Person
 {
+    private String favGame;
+
+    public Gamer() {}
+
+    public Gamer(String name, int ssn, String favGame)
+    {
+        super(name, ssn);
+        this.favGame = favGame;
+    }
+
+    public String getFavGame()
+    {
+        return favGame;
+    }
+    public void setFavGame(String favGame)
+    {
+        this.favGame = favGame;
+    }
+
+    @Override
+    public void greet()
+    {
+        System.out.printf("My name is %s and I LOVE playing games! My favourite is %s!\n", this.getName(), this.getFavGame());
+    }
 }
