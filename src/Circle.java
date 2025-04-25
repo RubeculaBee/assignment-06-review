8a913accc33a59963208a7a5d69d865961ecc3b6
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-04-25 12:27:35 -0700

    Implemented Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,41 @@
-public class Circle
+public class Circle implements Comparable<Object>
 {
+    private int radius;
+
+    public Circle()
+    {
+    }
+
+    public Circle(int radius)
+    {
+        this.radius = radius;
+    }
+
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public void setRadius(int radius)
+    {
+        this.radius = radius;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        if (this.getClass() != object.getClass())
+            return false;
+
+        return this.radius == ((Circle) object).radius;
+    }
+
+    @Override
+    public int compareTo(Object object)
+    {
+        if(this.getClass() != object.getClass())
+            throw new ClassCastException();
+
+        return this.getRadius() - ((Circle) object).getRadius();
+    }
 }
